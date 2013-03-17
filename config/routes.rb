512cca66ea5da0317721a627c9f1ef8aac748204
Stefan Kendall <stefankendall@gmail.com>
2013-03-17 15:44:50 -0400

    Added simple form input for email addresses to index.

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,8 @@ Letstradellc::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'simple#index'
+  resources :users
 
   # See how all your routes lay out with "rake routes"
 
