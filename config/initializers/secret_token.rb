fb381032040d85a01c3777f7287d7b3ea440ef3e
Stefan Kendall <stefankendall@gmail.com>
2013-03-17 14:46:27 -0400

    Switching app to rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letstradellc::Application.config.secret_token = '9c32fa8c88e305d30bddaf24dd02d6756f0f2be94f27fd44997c04a7395f2dd090ed64f74817a81a46977ab9697e71d2a996b0bf3b6ae4af6ab9a627f7197411'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Letstradellc::Application.config.session_store :cookie_store, key: '_letstradellc_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Letstradellc::Application.config.session_store :active_record_store
